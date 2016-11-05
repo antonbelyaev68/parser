7999c812d9251f3167b8c291c30a936656d4190c
antonbelyaev68 <antonbelyaev68@gmail.com>
2016-11-05 22:03:45 +0300

    parsing listsource

--- src/App/Services/ParserListsource.php
+++ src/App/Services/ParserListsource.php
@@ -18,7 +18,7 @@ class ParserListsource extends Parser
 
     public function parse()
     {
-        $driver = new Selenium2Driver('chrome', null, 'http://192.168.99.100:32795/wd/hub');
+        $driver = new Selenium2Driver('chrome', null, 'http://192.168.99.100:32777/wd/hub');
         $this->session = new Session($driver);
         $this->session->start();
         $this->session->visit($this->url);
@@ -49,31 +49,127 @@ class ParserListsource extends Parser
         $zipTextArea->setValue($this->zipCode);
 
         $button = $page->find('xpath', '//button');
-        $button->click();
+        $button->click(); // add zip code
 
         $js = null;
-        while (empty($zipTextArea)) { // working javascript
-            $js= $page->find('named', ['id_or_name', 'CRITERIA_ZIP_CODE_30236']);
+        while (empty($js)) { // working javascript
+            $js= $page->find('named', ['id_or_name', 'CRITERIA_ZIP_CODE_'.$this->zipCode]);
             $this->session->wait(1);
         }
 
-        $propertyIcon = $page->find('named', ['id_or_name', 'img_PROPERTY']);
-        $propertyIcon->click();
-
-        $propertyLink = $page->find('named', ['id_or_name', 'PROPERTY_PAGE_IMG']);
-        $propertyLink->click();
+        $propertyLink = $page->find('named', ['id_or_name', 'img_PROPERTY']);
+        $propertyLink->click(); // click property in left menu
         $this->session->wait(1);
 
+        $propertyLink = $page->find('named', ['id_or_name', 'PROPERTY_PAGE_IMG']);
+        $propertyLink->click(); // click tab property
+        $this->session->wait(1);
 
-        VarDumper::dump($button->getOuterHtml());
+        $criteria = $page->find('named', ['id_or_name', 'locator_prop']);
+        $criteria->selectOption("PROPERTY_TYPE");
 
-        #file_put_contents("scrin.png", $this->session->getScreenshot());
+        $locatorAvailableList_prop = null;
+        while (empty($locatorAvailableList_prop)) { // working javascript
+            $locatorAvailableList_prop = $page->find('named', ['id_or_name', 'locatorAvailableList_prop']);
+            $this->session->wait(1);
+        }
+        $this->session->wait(5);
+
+        file_put_contents("scrin.png", $this->session->getScreenshot());
+        VarDumper::dump($locatorAvailableList_prop->getOuterHtml());
+
+        $count = $this->parceCount($page);
+
+        $locatorAvailableList_prop->selectOption(16, true);
+        $prop_add = $page->find('named', ['id_or_name', 'prop_add']);
+        $prop_add->click();
+        $this->session->wait(5);
+
+
+        $countNew = $count;
+        while ($count == $countNew) { // working javascript
+            $countNew = $this->parceCount($page);
+            $this->session->wait(5);
+        }
+
+        $countNewNew = 0;
+        $countOldOld = 1;
+        while ($countNewNew != $countOldOld) { // working javascript
+            $countOldOld = $this->parceCount($page);
+            $this->session->wait(5);
+            $countNewNew = $this->parceCount($page);
+        }
+
+        $result['property_count'] = $countNewNew; //F in excel
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        $foreClosureLink = $page->find('named', ['id_or_name', 'FORECLOSURE_PAGE_IMG']);
+        $foreClosureLink->click();
+        $this->session->wait(1);
+
+        $foreClosureSelect = $page->find('named', ['id_or_name', 'locator_foreclosure']);
+        $foreClosureSelect->selectOption('FORE_D_PUB_DT'); // select recent added date
+
+        $last6month = null;
+        while (empty($last6month)) { // working javascript
+            $last6month= $page->find('named', ['id_or_name', 'last6_FORE_D_PUB_DT']);
+            $this->session->wait(1);
+        }
+
+        $last6month->click();
+        $this->session->wait(1);
+
+        $addButton = $page->find('named', ['id_or_name', 'mort_add_FORE_D_PUB_DT']);
+        $addButton->click();
+
+        $dateInputFrom = $page->find('named', ['id_or_name', 'fromValue_FORE_D_PUB_DT']);
+        $dateInputFromValue = $dateInputFrom->getValue();
+        $dateInputTo = $page->find('named', ['id_or_name', 'toValue_FORE_D_PUB_DT']);
+        $dateInputToValue = $dateInputTo->getValue();
+
+        $js = null;
+        while (empty($js)) { // working javascript
+            $js= $page->find('named', ['id_or_name', 'CRITERIA_FORE_D_PUB_DT_'.$dateInputFromValue.'-'.$dateInputToValue]);
+            $this->session->wait(1);
+        }
+
+        file_put_contents("scrin.png", $this->session->getScreenshot());
+        VarDumper::dump($js->getOuterHtml());
 
         exit;
 
         VarDumper::dump($key);
         VarDumper::dump($a->getXpath());
         VarDumper::dump($a->getOuterHtml());
+    }
+
+    private function parceCount($page)
+    {
+        $sing1 = $page->find('named', ['id_or_name', 'td_0']);
+        $sing1 = $this->getTextValue($sing1);
+        $sing2 = $page->find('named', ['id_or_name', 'td_1']);
+        $sing2 = $this->getTextValue($sing2);
+        #$sing3 = $page->find('named', ['id_or_name', 'td_2']);
+        #$sing3 = $sing3->getText();
+        $sing4 = $page->find('named', ['id_or_name', 'td_3']);
+        $sing4 = $this->getTextValue($sing4);
+        $sing5 = $page->find('named', ['id_or_name', 'td_4']);
+        $sing5 = $this->getTextValue($sing5);
+        $sing6 = $page->find('named', ['id_or_name', 'td_5']);
+        $sing6 = $this->getTextValue($sing6);
+
+        return $sing1.$sing2.$sing4.$sing5.$sing6;
+    }
+
+    private function getTextValue($sing)
+    {
+        if ($sing instanceof NodeElement) {
+            $val = $sing->getText();
+        } else {
+            $val = 0;
+        }
+        return $val;
     }
 
 
